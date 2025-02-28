ca13cb3068fd6799ec66910a4d51499354fca3ce
sonatanpaul <iamsonatanpaul@gmail.com>
2025-02-28 16:55:17 +0600

    learn document property

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-// Get ElementbyId
-const h1 = document.getElementById("hello");
-console.log(h1);
-
-document.getElementById("header").style.color = "blue";
-document.getElementById("header").style.backgroundColor = "yellow";
-document.getElementById("header").style.fontSize = "50px";
-document.getElementById("header").style.padding = "50px";
-
-// console.log(document.getElementsByClassName("heading"));
-// heading.style.color = "blue";
-
-// const className = document.getElementsByClassName("heading");
-// className.style.color = "blue";
-// console.log(className);
-
-document.getElementsByClassName("color").style.color = "blue";
+// console.log(document);
+// console.log(typeof document);  // object
+// console.dir(document.title);
+// console.log(document.URL);
+// console.log(document.domain);
+// console.log(document.all);
+// console.log(document.forms);
