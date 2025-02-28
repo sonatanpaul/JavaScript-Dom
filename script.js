c8a4a1f9dcd7f9acae66f8ec1fcb7dd6538c4bd7
sonatanpaul <iamsonatanpaul@gmail.com>
2025-02-28 12:40:19 +0600

    learn dom selector

---
+++ script.js
@@ -0,0 +1,17 @@
+// Get ElementbyId
+const h1 = document.getElementById("hello");
+console.log(h1);
+
+document.getElementById("header").style.color = "blue";
+document.getElementById("header").style.backgroundColor = "yellow";
+document.getElementById("header").style.fontSize = "50px";
+document.getElementById("header").style.padding = "50px";
+
+// console.log(document.getElementsByClassName("heading"));
+// heading.style.color = "blue";
+
+// const className = document.getElementsByClassName("heading");
+// className.style.color = "blue";
+// console.log(className);
+
+document.getElementsByClassName("color").style.color = "blue";
